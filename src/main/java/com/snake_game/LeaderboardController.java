0eab4123c0fb26ce48652ac2b40957617b722285
psyjm27 <psyjm27@nottingham.ac.uk>
2023-12-06 02:30:41 +0000

    feat:Added back button to leaderboard view

--- src/main/java/com/snake_game/LeaderboardController.java
+++ src/main/java/com/snake_game/LeaderboardController.java
@@ -1,8 +1,14 @@
 package com.snake_game;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class LeaderboardController {
 
@@ -15,4 +21,11 @@ public class LeaderboardController {
     @FXML
     private TableView<?> table;
 
+    @FXML
+    void backtoMenu(ActionEvent event) throws IOException {
+        FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("/Views/menu-view.fxml"));
+        Scene scene = new Scene(menuLoader.load());
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+    }
 }

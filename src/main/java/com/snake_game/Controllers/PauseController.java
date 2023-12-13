794e1168d3cae8263a17533a1f6822a90ff8adc8
Joshua Muiruri <josh023@hotmail.co.uk>
2023-12-13 22:41:03 +0000

    feat: Created Pause screen and controller pause-view.fxml and PauseController.java are the files created

---
+++ src/main/java/com/snake_game/Controllers/PauseController.java
@@ -0,0 +1,18 @@
+package com.snake_game.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class PauseController {
+
+    @FXML
+    void quitGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resumeGame(ActionEvent event) {
+
+    }
+
+}
